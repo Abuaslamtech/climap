2ad7b007a670d1d6025e5934e5b6e6103da94716
Abdullahi Ismail <abdullahabuaslam@gmail.com>
2025-01-20 23:01:26 +0100

    refactor ProtectedRoute component to manage authentication state and redirect users based on token presence

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const isLoggedIn = null;
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+
+  const token = localStorage.getItem(token);
+  console.log(token);
+  if (token) {
+    setisAuthenticated(true);
+  } else {
+    setisAuthenticated(false);
+  }
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
