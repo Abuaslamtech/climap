362a19013f1bbbc344b3faad91c359bcc49aed44
Abdullahi Ismail <abdullahabuaslam@gmail.com>
2025-01-23 19:56:46 +0100

    enhance authentication middleware with detailed error logging and token validation feedback

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,17 +1,23 @@
-import jwt from "jsonwebtoken";
-
 export const authenticateUser = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+  console.log("Received token:", token);
+
   if (!token) {
     return res
       .status(403)
       .json({ message: "Access Denied, no token provided" });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Token invalid, access denied" });
+    console.error("Verification error details:", err);
+    res.status(401).json({
+      message: "Token invalid, access denied",
+      error: err.message,
+    });
   }
 };
